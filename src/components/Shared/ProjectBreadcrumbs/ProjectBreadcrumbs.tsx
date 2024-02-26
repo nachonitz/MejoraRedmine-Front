053e5b41e5454eb7f6179931d36ecac36a0457c6
Ignacio Nitz <Nachonitz@gmail.com>
2024-02-25 22:39:47 -0300

    Fixed project breadcrumbs

--- src/components/Shared/ProjectBreadcrumbs/ProjectBreadcrumbs.tsx
+++ src/components/Shared/ProjectBreadcrumbs/ProjectBreadcrumbs.tsx
@@ -7,72 +7,72 @@ import { Release } from "../../../api/models/release";
 import { Sprint } from "../../../api/models/sprint";
 
 interface ProjectBreadcrumbsProps {
-  project?: Project;
-  release?: Release;
-  sprint?: Sprint;
-  epic?: Epic;
+    project?: Project;
+    release?: Release;
+    sprint?: Sprint;
+    epic?: Epic;
 }
 
 const ProjectBreadcrumbs = ({
-  project,
-  release,
-  sprint,
-  epic,
+    project,
+    release,
+    sprint,
+    epic,
 }: ProjectBreadcrumbsProps) => {
-  return (
-    <div className="mb-5">
-      <Breadcrumbs aria-label="breadcrumb">
-        <Link
-          underline="hover"
-          color="inherit"
-          component={ReactRouterLink}
-          to="/projects"
-        >
-          Projects
-        </Link>
-        {project && (
-          <Link
-            underline="hover"
-            color="inherit"
-            component={ReactRouterLink}
-            to={`/project/${project.id}`}
-          >
-            {project.name}
-          </Link>
-        )}
-        {release && project && (
-          <Link
-            underline="hover"
-            color="inherit"
-            component={ReactRouterLink}
-            to={`/project/${project.id}/release/${release.id}`}
-          >
-            {release.name}
-          </Link>
-        )}
-        {release && project && sprint && (
-          <Link
-            underline="hover"
-            color="inherit"
-            component={ReactRouterLink}
-            to={`/project/${project.id}/release/${release.id}/sprint/${sprint.id}`}
-          >
-            {sprint.name}
-          </Link>
-        )}
-        {release && project && sprint && epic && (
-          <Link
-            underline="hover"
-            color="inherit"
-            component={ReactRouterLink}
-            to={`/project/${project.id}/release/${release.id}/sprint/${sprint.id}/epic/${epic.id}`}
-          >
-            {epic.name}
-          </Link>
-        )}
-      </Breadcrumbs>
-    </div>
-  );
+    return (
+        <div className="mb-5">
+            <Breadcrumbs aria-label="breadcrumb">
+                <Link
+                    underline="hover"
+                    color="inherit"
+                    component={ReactRouterLink}
+                    to="/projects"
+                >
+                    Projects
+                </Link>
+                {project && (
+                    <Link
+                        underline="hover"
+                        color="inherit"
+                        component={ReactRouterLink}
+                        to={`/project/${project.id}/overview`}
+                    >
+                        {project.name}
+                    </Link>
+                )}
+                {release && project && (
+                    <Link
+                        underline="hover"
+                        color="inherit"
+                        component={ReactRouterLink}
+                        to={`/project/${project.id}/overview/release/${release.id}`}
+                    >
+                        {release.name}
+                    </Link>
+                )}
+                {release && project && sprint && (
+                    <Link
+                        underline="hover"
+                        color="inherit"
+                        component={ReactRouterLink}
+                        to={`/project/${project.id}/overview/release/${release.id}/sprint/${sprint.id}`}
+                    >
+                        {sprint.name}
+                    </Link>
+                )}
+                {release && project && sprint && epic && (
+                    <Link
+                        underline="hover"
+                        color="inherit"
+                        component={ReactRouterLink}
+                        to={`/project/${project.id}/overview/release/${release.id}/sprint/${sprint.id}/epic/${epic.id}`}
+                    >
+                        {epic.name}
+                    </Link>
+                )}
+            </Breadcrumbs>
+        </div>
+    );
 };
 
 export default ProjectBreadcrumbs;
